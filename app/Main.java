74ce7efc02aad8a2e61d49f3f939bacd1b9574b5
andreluisoliveirac <contatcandreluis@gmail.com>
2023-07-01 22:13:28 -0300

    Atualização

--- app/Main.java
+++ app/Main.java
@@ -1,4 +1,18 @@
+import br.com.alura.screenmatch.modelos.App;
 
 public class Main {
+    public static void main(String[] args) {
+        App meuFilme = new App();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLacamento(1980);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setInlcuidoNoPlano(true);
 
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.obtemMedia());        
+    }
 }
--- src/br/com/alura/screenmatch/modelos/App.java
+++ src/br/com/alura/screenmatch/modelos/App.java
@@ -1,10 +1,74 @@
 package br.com.alura.screenmatch.modelos;
 
 public class App {
+    private String nome;
+    private int anoDeLacamento;
+    private boolean inlcuidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
 
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLacamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double obtemMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+
+    }
+
+	public String getNome() {
+		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAnoDeLacamento() {
+		return anoDeLacamento;
+	}
+
+	public void setAnoDeLacamento(int anoDeLacamento) {
+		this.anoDeLacamento = anoDeLacamento;
+	}
+
+	public boolean isInlcuidoNoPlano() {
+		return inlcuidoNoPlano;
+	}
+
+	public void setInlcuidoNoPlano(boolean inlcuidoNoPlano) {
+		this.inlcuidoNoPlano = inlcuidoNoPlano;
+	}
+
+	public double getSomaDasAvaliacoes() {
+		return somaDasAvaliacoes;
+	}
+
+	public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+		this.somaDasAvaliacoes = somaDasAvaliacoes;
+	}
+
+	public int getDuracaoEmMinutos() {
+		return duracaoEmMinutos;
+	}
+
+	public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+		this.duracaoEmMinutos = duracaoEmMinutos;
+	}
+
+	public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+		this.totalDeAvaliacoes = totalDeAvaliacoes;
+	}
 }
